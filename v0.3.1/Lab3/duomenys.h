4c79e5a4ff3d974185c3e68abdcc048421597dd9
MarkasGen <114186789+MarkasGen@users.noreply.github.com>
2022-11-07 23:35:30 +0200

    Add files via upload

---
+++ v0.3.1/Lab3/duomenys.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+struct duomenys {
+    std::string Vardas, Pavarde;
+    std::vector<int> paz = { 0 };
+    int egz;
+    float GP = 0;
+};
